74779991eae259dc4397c8fb75555179cec381ab
StevenGerrad <1339807619@qq.com>
2023-03-07 11:27:49 +0800

    p27 秒杀功能实现

--- src/main/java/com/xxxx/seckill/vo/GoodsVo.java
+++ src/main/java/com/xxxx/seckill/vo/GoodsVo.java
@@ -12,6 +12,9 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GoodsVo extends Goods {
+    // 【弹幕debug：有的人说不能继承父类的private属性】，但实际上（根据菜鸟教程-Java）：
+    // 子类和父类在同一个包中，那么子类可以重写父类所有方法，除了声明为 private 和 final 的方法。
+    // 子类和父类不在同一个包中，那么子类只能够重写父类的声明为 public 和 protected 的非 final 方法。
     private BigDecimal seckillPrice;
     private Integer stockCount;
 
