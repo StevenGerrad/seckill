bfef49950ef8823fba398972d5621169af098083
StevenGerrad <1339807619@qq.com>
2023-05-03 20:36:19 +0800

    p39 商品详情页面静态化：用ajax拆分

--- src/main/java/com/xxxx/seckill/controller/SecKillController.java
+++ src/main/java/com/xxxx/seckill/controller/SecKillController.java
@@ -9,11 +9,14 @@ import com.xxxx.seckill.service.IGoodsService;
 import com.xxxx.seckill.service.IOrderService;
 import com.xxxx.seckill.service.ISeckillOrderService;
 import com.xxxx.seckill.vo.GoodsVo;
+import com.xxxx.seckill.vo.RespBean;
 import com.xxxx.seckill.vo.RespBeanEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/seckill")
@@ -40,8 +43,8 @@ public class SecKillController {
      * @author Administrator
      * @date 2023/4/22 21:51
      */
-    @RequestMapping("/doSeckill")
-    public String doSecKill(Model model, User user, Long goodsId){
+    @RequestMapping("/doSeckill2")
+    public String doSecKill2(Model model, User user, Long goodsId){
         if(user == null){
             return "login";
         }
@@ -67,5 +70,29 @@ public class SecKillController {
 
         // TODO：看一下这个spring ui Model 是啥，怎么什么都往里加
         return "orderDetail";
+    }
+
+    @RequestMapping(value = "/doSeckill", method = RequestMethod.POST)
+    @ResponseBody
+    public RespBean doSecKill(Model model, User user, Long goodsId){
+        if(user == null){
+            return RespBean.error(RespBeanEnum.SESSION_ERROR);
+        }
+        GoodsVo goods = goodsService.findGoodsVoByGoodsId(goodsId);
+        // 判断库存
+        if(goods.getStockCount() < 1){
+            model.addAttribute("errmsg", RespBeanEnum.EMPTY_STOCK.getMessage());
+            return RespBean.error(RespBeanEnum.EMPTY_STOCK);
+        }
+        // 判断是否重复抢购
+        SeckillOrder seckillOrder = seckillOrderService.getOne(new QueryWrapper<SeckillOrder>().eq("user_id",
+                user.getId()).eq("goods_id", goodsId));
+        if(seckillOrder != null) {
+            model.addAttribute("errmsg", RespBeanEnum.REPEATE_ERROR.getMessage());
+            return RespBean.error(RespBeanEnum.REPEATE_ERROR);
+        }
+        Order order = orderService.seckill(user, goods);
+
+        return RespBean.success(order);
     }
 }
