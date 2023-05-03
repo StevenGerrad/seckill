c1830bd9e60dd7f532e2862d666ae3c49564f778
StevenGerrad <1339807619@qq.com>
2023-05-03 21:52:57 +0800

    p42 解决库存超卖：修改sql语句+表中加唯一索引+改用redis判断是否重复抢购

--- src/main/java/com/xxxx/seckill/controller/SecKillController.java
+++ src/main/java/com/xxxx/seckill/controller/SecKillController.java
@@ -12,6 +12,7 @@ import com.xxxx.seckill.vo.GoodsVo;
 import com.xxxx.seckill.vo.RespBean;
 import com.xxxx.seckill.vo.RespBeanEnum;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,11 +29,14 @@ public class SecKillController {
 
     @Autowired
     private IOrderService orderService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * @description
      * windows 优化前(1000 kernal * 1) QPS: 30.3
      * windows 优化前(1000 kernal * 3) QPS: 29.9 (注意要设置循环数，才会出现库存为负数的问题)
+     *         缓存优化(1000 * 3) QPS: 556.8
      * @param model
      * defaultParamDescription
      * @param user
@@ -72,6 +76,30 @@ public class SecKillController {
         return "orderDetail";
     }
 
+    // @RequestMapping(value = "/doSeckill", method = RequestMethod.POST)
+    // @ResponseBody
+    // public RespBean doSecKill(Model model, User user, Long goodsId){
+    //     if(user == null){
+    //         return RespBean.error(RespBeanEnum.SESSION_ERROR);
+    //     }
+    //     GoodsVo goods = goodsService.findGoodsVoByGoodsId(goodsId);
+    //     // 判断库存
+    //     if(goods.getStockCount() < 1){
+    //         model.addAttribute("errmsg", RespBeanEnum.EMPTY_STOCK.getMessage());
+    //         return RespBean.error(RespBeanEnum.EMPTY_STOCK);
+    //     }
+    //     // 判断是否重复抢购
+    //     SeckillOrder seckillOrder = seckillOrderService.getOne(new QueryWrapper<SeckillOrder>().eq("user_id",
+    //             user.getId()).eq("goods_id", goodsId));
+    //     if(seckillOrder != null) {
+    //         model.addAttribute("errmsg", RespBeanEnum.REPEATE_ERROR.getMessage());
+    //         return RespBean.error(RespBeanEnum.REPEATE_ERROR);
+    //     }
+    //     Order order = orderService.seckill(user, goods);
+    //
+    //     return RespBean.success(order);
+    // }
+
     @RequestMapping(value = "/doSeckill", method = RequestMethod.POST)
     @ResponseBody
     public RespBean doSecKill(Model model, User user, Long goodsId){
@@ -85,8 +113,9 @@ public class SecKillController {
             return RespBean.error(RespBeanEnum.EMPTY_STOCK);
         }
         // 判断是否重复抢购
-        SeckillOrder seckillOrder = seckillOrderService.getOne(new QueryWrapper<SeckillOrder>().eq("user_id",
-                user.getId()).eq("goods_id", goodsId));
+        // SeckillOrder seckillOrder = seckillOrderService.getOne(new QueryWrapper<SeckillOrder>().eq("user_id",
+        //         user.getId()).eq("goods_id", goodsId));
+        SeckillOrder seckillOrder = (SeckillOrder) redisTemplate.opsForValue().get("order:" + user.getId() + ":" + goods.getId());
         if(seckillOrder != null) {
             model.addAttribute("errmsg", RespBeanEnum.REPEATE_ERROR.getMessage());
             return RespBean.error(RespBeanEnum.REPEATE_ERROR);
--- src/main/java/com/xxxx/seckill/service/impl/OrderServiceImpl.java
+++ src/main/java/com/xxxx/seckill/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.xxxx.seckill.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xxxx.seckill.exception.GlobalException;
 import com.xxxx.seckill.mapper.OrderMapper;
@@ -16,7 +17,9 @@ import com.xxxx.seckill.vo.GoodsVo;
 import com.xxxx.seckill.vo.OrderDetailVo;
 import com.xxxx.seckill.vo.RespBeanEnum;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
@@ -37,17 +40,60 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     private OrderMapper orderMapper;
     @Autowired
     private ISeckillOrderService seckillOrderService;
-
     @Autowired
     private IGoodsService goodsService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
+    // @Override
+    // public Order seckill(User user, GoodsVo goods){
+    //     // 秒杀商品减库存
+    //     SeckillGoods seckillGoods = seckillGoodsService.getOne(new QueryWrapper<SeckillGoods>().eq("goods_id", goods.getId()));
+    //     seckillGoods.setStockCount(seckillGoods.getStockCount() - 1);
+    //     // TODO：这里直接用raw的sql语句？包装成事务？（这里实际上是没有用到事务的概念的吧）
+    //     seckillGoodsService.updateById(seckillGoods);
+    //
+    //     // 生成订单
+    //     Order order = new Order();
+    //     order.setUserId(user.getId());
+    //     order.setGoodsId(goods.getId());
+    //     order.setDeliveryAddrId(0L);
+    //     order.setGoodsName(goods.getGoodsName());
+    //     order.setGoodsCount(1);
+    //     order.setGoodsPrice(seckillGoods.getSeckillPrice());
+    //     order.setOrderChannel(1);
+    //     order.setStatus(0);
+    //     order.setCreateDate(new Date());
+    //     orderMapper.insert(order);
+    //
+    //     // 生成秒杀订单
+    //     SeckillOrder seckillOrder = new SeckillOrder();
+    //     seckillOrder.setUserId(user.getId());
+    //     seckillOrder.setOrderId(order.getId());
+    //     seckillOrder.setGoodsId(goods.getId());
+    //     seckillOrderService.save(seckillOrder);
+    //
+    //     return order;
+    // }
+
+    @Transactional
     @Override
     public Order seckill(User user, GoodsVo goods){
         // 秒杀商品减库存
         SeckillGoods seckillGoods = seckillGoodsService.getOne(new QueryWrapper<SeckillGoods>().eq("goods_id", goods.getId()));
         seckillGoods.setStockCount(seckillGoods.getStockCount() - 1);
-        // TODO：这里直接用raw的sql语句？包装成事务？（这里实际上是没有用到事务的概念的吧）
-        seckillGoodsService.updateById(seckillGoods);
+        // boolean seckillGoodsResult = seckillGoodsService.update(new UpdateWrapper<SeckillGoods>().set("stock_count",
+        //         seckillGoods.getStockCount()).eq("id", seckillGoods.getId()).gt("stock_count", 0));
+
+        // boolean seckillGoodsResult = seckillGoodsService.update(new UpdateWrapper<SeckillGoods>().setSql(
+        //         "stock_count = stock_count-1").eq("goods_id", goods.getId()));
+        // if(seckillGoods.getStockCount() < 1) return null;
+
+        // 改为sql的update语句
+        boolean seckillGoodsResult = seckillGoodsService.update(new UpdateWrapper<SeckillGoods>().setSql(
+                "stock_count = stock_count-1").eq("goods_id", goods.getId()).gt("stock_count", 0));
+        if(!seckillGoodsResult) return null;
+
 
         // 生成订单
         Order order = new Order();
@@ -68,6 +114,8 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
         seckillOrder.setOrderId(order.getId());
         seckillOrder.setGoodsId(goods.getId());
         seckillOrderService.save(seckillOrder);
+
+        redisTemplate.opsForValue().set("order:" + user.getId() + ":" + goods.getId(), seckillOrder);
 
         return order;
     }
