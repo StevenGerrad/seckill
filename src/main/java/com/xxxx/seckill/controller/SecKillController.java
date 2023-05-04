cca2823b40197fff6afa54b1b75697affe28b082
StevenGerrad <1339807619@qq.com>
2023-05-04 20:22:56 +0800

    p59 秒杀接口地址隐藏

--- src/main/java/com/xxxx/seckill/controller/SecKillController.java
+++ src/main/java/com/xxxx/seckill/controller/SecKillController.java
@@ -22,6 +22,7 @@ import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -177,13 +178,80 @@ public class SecKillController implements InitializingBean {
     // }
 
     // P56
-    @RequestMapping(value = "/doSeckill", method = RequestMethod.POST)
+    // @RequestMapping(value = "/doSeckill", method = RequestMethod.POST)
+    // @ResponseBody
+    // public RespBean doSecKill(Model model, User user, Long goodsId){
+    //     if(user == null){
+    //         return RespBean.error(RespBeanEnum.SESSION_ERROR);
+    //     }
+    //     ValueOperations valueOperations = redisTemplate.opsForValue();
+    //     // 判断是否重复抢购
+    //     SeckillOrder seckillOrder = (SeckillOrder) redisTemplate.opsForValue().get("order:" + user.getId() + ":" + goodsId);
+    //     if(seckillOrder != null) {
+    //         return RespBean.error(RespBeanEnum.REPEATE_ERROR);
+    //     }
+    //     if(EmptyStockMap.get(goodsId)){
+    //         return RespBean.error(RespBeanEnum.EMPTY_STOCK);
+    //     }
+    //     // 预减库存
+    //     // Long stock = valueOperations.decrement("seckillGoods:" + goodsId);
+    //     Long stock = (Long) redisTemplate.execute(script, Collections.singletonList("seckillGoods:" + goodsId), Collections.EMPTY_LIST);
+    //     if(stock < 0){
+    //         EmptyStockMap.put(goodsId, true);
+    //         valueOperations.increment("seckillGoods:" + goodsId);
+    //         return RespBean.error(RespBeanEnum.EMPTY_STOCK);
+    //     }
+    //     SeckillMessage seckillMessage = new SeckillMessage(user, goodsId);
+    //     // 使用RabbitMq可以达到流量削峰的效果
+    //     mqSender.sendSeckillMessage(JsonUtil.object2JsonStr(seckillMessage));
+    //
+    //     return RespBean.success(0);
+    // }
+
+    @Override
+    public void afterPropertiesSet() throws Exception{
+        // 将库存加载到redis
+        List<GoodsVo> list = goodsService.findGoodsVo();
+        if(CollectionUtils.isEmpty(list)) return ;
+        list.forEach(goodsVo -> {
+            redisTemplate.opsForValue().set("seckillGoods:" + goodsVo.getId(), goodsVo.getStockCount());
+            EmptyStockMap.put(goodsVo.getId(), false);
+        });
+    }
+
+    @RequestMapping(value = "/result", method = RequestMethod.GET)
     @ResponseBody
-    public RespBean doSecKill(Model model, User user, Long goodsId){
+    public RespBean getResult(User user, Long goodsId){
+        if(user == null) return RespBean.error(RespBeanEnum.SESSION_ERROR);
+        Long orderId = seckillOrderService.getResult(user, goodsId);
+        return RespBean.success(orderId);
+    }
+
+    @RequestMapping(value = "/path", method = RequestMethod.GET)
+    @ResponseBody
+    public RespBean getPath(User user, Long goodsId){
+        if(user == null){
+            return RespBean.error(RespBeanEnum.SESSION_ERROR);
+        }
+        String str = orderService.createPath(user, goodsId);
+        return RespBean.success(str);
+    }
+
+    // P59
+    @RequestMapping(value = "/{path}/doSeckill", method = RequestMethod.POST)
+    @ResponseBody
+    public RespBean doSecKill(@PathVariable String path, User user, Long goodsId){
         if(user == null){
             return RespBean.error(RespBeanEnum.SESSION_ERROR);
         }
         ValueOperations valueOperations = redisTemplate.opsForValue();
+
+        // P59，校验秒杀地址
+        boolean check = orderService.checkPath(user, goodsId, path);
+        if(!check){
+            return RespBean.error(RespBeanEnum.REQUEST_ILLEGAL);
+        }
+
         // 判断是否重复抢购
         SeckillOrder seckillOrder = (SeckillOrder) redisTemplate.opsForValue().get("order:" + user.getId() + ":" + goodsId);
         if(seckillOrder != null) {
@@ -205,24 +273,5 @@ public class SecKillController implements InitializingBean {
         mqSender.sendSeckillMessage(JsonUtil.object2JsonStr(seckillMessage));
 
         return RespBean.success(0);
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception{
-        // 将库存加载到redis
-        List<GoodsVo> list = goodsService.findGoodsVo();
-        if(CollectionUtils.isEmpty(list)) return ;
-        list.forEach(goodsVo -> {
-            redisTemplate.opsForValue().set("seckillGoods:" + goodsVo.getId(), goodsVo.getStockCount());
-            EmptyStockMap.put(goodsVo.getId(), false);
-        });
-    }
-
-    @RequestMapping(value = "/result", method = RequestMethod.GET)
-    @ResponseBody
-    public RespBean getResult(User user, Long goodsId){
-        if(user == null) return RespBean.error(RespBeanEnum.SESSION_ERROR);
-        Long orderId = seckillOrderService.getResult(user, goodsId);
-        return RespBean.success(orderId);
     }
 }
