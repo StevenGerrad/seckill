a9a635198877ef3cab87d5fe3399a3ce45dd3cbd
StevenGerrad <1339807619@qq.com>
2023-05-04 11:38:20 +0800

    p51 headers模式，编写比较复杂，效率低

--- src/main/java/com/xxxx/seckill/controller/UserController.java
+++ src/main/java/com/xxxx/seckill/controller/UserController.java
@@ -79,4 +79,16 @@ public class UserController {
     public void mq05(){
         mqSender.send04("Hello, Green");
     }
+
+    @RequestMapping("/mq/header01")
+    @ResponseBody
+    public void mq06(){
+        mqSender.send05("Hello, Header01");
+    }
+
+    @RequestMapping("/mq/header02")
+    @ResponseBody
+    public void mq07(){
+        mqSender.send06("Hello, Header02");
+    }
 }
--- src/main/java/com/xxxx/seckill/rabbitmq/MQSender.java
+++ src/main/java/com/xxxx/seckill/rabbitmq/MQSender.java
@@ -1,6 +1,9 @@
 package com.xxxx.seckill.rabbitmq;
 
+
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,14 +33,30 @@ public class MQSender {
 
     public void send03(Object msg){
         log.info("发送消息(Queue01接收)" + msg);
-        // 发消息时携带的路由key必须和刚刚绑定的一样
         rabbitTemplate.convertAndSend("topicExchange","queue.red.message", msg);
     }
 
     public void send04(Object msg){
         log.info("发送消息(被两个Queue接收)" + msg);
-        // 发消息时携带的路由key必须和刚刚绑定的一样
         rabbitTemplate.convertAndSend("topicExchange","green.queue.message", msg);
+    }
+
+    public void send05(String msg){
+        log.info("发送消息(被两个Queue接收)" + msg);
+        MessageProperties properties = new MessageProperties();
+        properties.setHeader("color", "red");
+        properties.setHeader("speed", "fast");
+        Message message = new Message(msg.getBytes(), properties);
+        rabbitTemplate.convertAndSend("headersExchange","", message);
+    }
+
+    public void send06(String msg){
+        log.info("发送消息(Queue01接收)" + msg);
+        MessageProperties properties = new MessageProperties();
+        properties.setHeader("color", "red");
+        properties.setHeader("speed", "normal");
+        Message message = new Message(msg.getBytes(), properties);
+        rabbitTemplate.convertAndSend("headersExchange","", message);
     }
 
 }
